e91aca64d6b37fd96d3ec8bd0c731e2fc2d5fb3d
batmanwgd <batmanwgd@gmail.com>
2020-05-06 16:18:18 -0400

    reorg

--- .eleventy.js
+++ .eleventy.js
@@ -39,6 +39,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("admin");
+  eleventyConfig.addPassthroughCopy("_dist");
 
   /* Markdown Overrides */
   let markdownLibrary = markdownIt({

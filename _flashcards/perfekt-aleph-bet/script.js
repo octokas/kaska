f506fc6bc778ad0dacf1cca05aea717aeccb8f0f
Batman <batmanwgd@gmail.com>
2020-05-24 22:19:11 -0400

    Flashcards more keys

--- _flashcards/perfekt-aleph-bet/script.js
+++ _flashcards/perfekt-aleph-bet/script.js
@@ -2,6 +2,7 @@ var num = 0;
 var shuffle = document.querySelector(".shuffle");
 var div = document.querySelector("#card");
 var eng = document.querySelector("#trans");
+var instr = document.querySelector("#instr").innerHTML = "Please use either the <b><u>up</u></b> and <b><u>down</u></b> arrow keys, OR use the <b><u>left</u></b> and <b><u>right</u></b> arrow keys to continue through the deck.";
 
 document.addEventListener("keydown", event => {
   const keyName = event.key;

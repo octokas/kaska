94ae7e7b46fa22cb0baab18f91ff835ef14767a0
Batman <batmanwgd@gmail.com>
2020-05-26 21:34:01 -0400

    yay homepage

---
+++ kaska-s-portfoliohomepage-with-menu-expander/README.markdown
@@ -0,0 +1,5 @@
+# Kaska's Portfolio - Homepage with Menu Expander
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/batmanwgd/pen/abvrpgZ](https://codepen.io/batmanwgd/pen/abvrpgZ).
+
+Pen is for my portfolio website which when finished will be at https://iamkaska.com/
---
+++ kaska-s-portfoliohomepage-with-menu-expander/dist/script.js
@@ -0,0 +1,11 @@
+if ('scrollRestoration' in history) {
+  // Back off, browser, I got this...
+  history.scrollRestoration = 'manual';
+}
+
+// $(".horizontal-scroll").wrapInner("<table cellspacing='0'><tr>");
+// $(".panel").wrap("<td></td>");
+$("body").mousewheel(function(event, delta) {
+  this.scrollLeft -= delta * 30;
+  event.preventDefault();
+});
